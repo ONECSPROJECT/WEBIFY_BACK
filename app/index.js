443d408d66f0469f8ca451695f515e385b3956b5
hxuu <133578197+hxuu@users.noreply.github.com>
2025-03-12 20:02:15 +0100

    Feature: add docs using SwaggerUI at /api-docs endpoint (config required)

--- app/index.js
+++ app/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const cors = require('cors');
+const path = require('node:path');
 const userRoutes = require('./routes/userRoutes');
 
 dotenv.config({ path: './config.env' });
@@ -12,6 +13,12 @@ const app = express();
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
+
+// SwaggerUI
+if (process.env.NODE_ENV !== 'prod') {
+    app.use('/api-docs/swagger.json', express.static(path.join(__dirname, 'config/swagger.json')));
+    app.use('/api-docs', express.static(path.join(__dirname, 'dist')));
+}
 
 app.use((req, res, next) => {
     console.log(`➡️ Incoming ${req.method} request to: ${req.url}`);
