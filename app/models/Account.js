34846690e8416b64407de3e6e4875a41240fe995
hxuu <133578197+hxuu@users.noreply.github.com>
2025-03-14 15:15:00 +0100

    return one account instead of multiple in Account.js findByEmail Query

--- app/models/Account.js
+++ app/models/Account.js
@@ -2,10 +2,11 @@ const pool = require('../config/db');
 
 class Account {
     static async findByEmail(email) {
-        return pool.query(
+        const [rows] =  pool.query(
             'SELECT * FROM Account WHERE email = ?',
             [email]
         )
+        return rows[0] || null; // could have multiple records
     }
 
     static async updatePassword(accountId, hashedPassword, salt) {
